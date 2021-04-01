934dd839e42a3f751aba3e64ef6ebcd8a3ba0b42
xWinkleAU <gamingkyle7@outlook.com>
2021-04-01 14:25:39 +1100

    Updated project files

--- CSCI251-Assignment1/main.cpp
+++ CSCI251-Assignment1/main.cpp
@@ -1,1 +1,38 @@
+/*
+* 
+*	Bachelor of Computer Science (Software Engineering) 
+*			
+*				Kyle J. Brookes | 6822794
+* 
+*					 Assignment 1
+* 
+*/
+
+
+#include <iostream>
 #include "main.h"
+
+using namespace std;
+
+class Subject{
+	public:
+		string name;
+		int difficulty;
+		int variability;
+};
+
+class Student {
+	public:
+		string name;
+		int studentID;
+		int ability;
+		int consistency;
+		Subject subjects[];
+};
+
+
+int main() {
+
+	
+
+}
--- CSCI251-Assignment1/main.h
+++ CSCI251-Assignment1/main.h
@@ -1,5 +1,12 @@
-#pragma once
-class main
-{
-};
+/*
+*
+*	Bachelor of Computer Science (Software Engineering)
+*
+*				Kyle J. Brookes | 6822794
+*
+*					 Assignment 1
+*
+*/
 
+
+int main();
